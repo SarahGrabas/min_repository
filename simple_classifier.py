915d7f043826c3e3360a9bfcaf88b7f7248511fc
SarahGrabas <sarahgrabas2@gmail.com>
2025-02-12 19:53:23 +0100

    hej

--- simple_classifier.py
+++ simple_classifier.py
@@ -4,7 +4,7 @@ from matplotlib.colors import ListedColormap
 from sklearn import datasets, neighbors
 from sklearn.inspection import DecisionBoundaryDisplay
 # Ny kommentar i ny branch
-#huheuhue
+#test
 
 if __name__ == "__main__":
     n_neighbors = 15
